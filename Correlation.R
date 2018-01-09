cfa540996b6ff646c66e7d12a5a49719e4570643
mlacher <mlacher@live.de>
2018-01-09 13:07:53 +0100

    2nd major update
    updated correlation function with additional explanations

--- Correlation.R
+++ Correlation.R
@@ -1,29 +1,34 @@
-onlygood <-INV_013all[INV_013all$`GAU HEAD Spectrum Category`=="OK",]
-y1<- onlygood$I_MaxMem
-x1<- onlygood$`Membrane weight in mg`
+#This program creates a linear correlation between two factors, furthermore the prediction and confidence intervall will be shown
+#MaL
 
+#1 Calulate linear regression
+x1<- '[##x_data##]'
+y1<- '[##y_data##]'
+modell <- lm(y1~x1)
+
+#2 Plot points and linear regression
 windows()
-modell <- lm(y1~x1,onlygood)
-
-
-plot(y1~x1,onlygood)
+plot(y1~x1)
 abline (modell, col = "green")
 summary(modell)
+
+#3 Plot correlation values
 windows()
 plot(  modell )
 savePlot(filename="MeinDiagramm.wmf", type="wmf")
 
+
+#4 Plot points, linear regression and prediction/confidence intervall
 windows()
-
-new <- data.frame(x1=seq(19,21,0.5)) '//sequence to simulate data'
-
-reg.pred.p <- predict(modell, new, interval="prediction")
+new <- data.frame(x1=seq('[##lower_x_axis##]','[##upper_x_axis##]','[##stepwidth##]')) 
+reg.pred.p <- predict(modell, new, interval="prediction") 
+#predict intervall (95% pred int), intervall from given points
 reg.pred.c <- predict(modell, new, interval="confidence")
-
+#confid intervall (95% confid. int), intervall from mean value of points
 matplot(new$x1, cbind(reg.pred.p, reg.pred.c[,-1]),
-        lty=c(1,2,2,3,3), col=c(1,2,2,3,3), type="l", ylab="Min membrane Frequency (Current)",xlab= "Membrane Weight")
-
+        lty=c(1,2,2,3,3), col=c(1,2,2,3,3), type="l", ylab="##y_lab##",xlab= "##x_lab##")
 points(x1,y1)
-
-legend(20,330, legend=c("predicted y", "pred. interval", "confidence interval"), lty=c(1,2,3), col=c(1,2,3),cex = 0.6)
+legend('[##legend_x_axis##]','[##legend_y_axis##]', 
+       legend=c("predicted y", "pred. interval", "confidence interval"), 
+       lty=c(1,2,3), col=c(1,2,3),cex = 0.6)
 
